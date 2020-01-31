1976a2f47a4b617d337fbd2a4dccbc443d956c0e
Ubuntu <ubuntu@ip-172-31-30-141.ec2.internal>
2020-01-31 02:08:50 +0000

    final project

---
+++ constants.py
@@ -0,0 +1,32 @@
+# CircuitPython screen size is 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 228
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_TOMATOS = 10
+TOTAL_NUMBER_OF_PIES = 10
+TOTAL_NUMBER_OF_BALLOONS = 10
+CLOWN_SPEED = 1
+TOMATO_SPEED = 1.5
+PIE_SPEED = 3
+BALLOON_SPEED = 2
+OFF_SCREEN_X = -100
+OFF_SCREEN_Y = -100
+OFF_TOP_SCREEN = -1 * SPRITE_SIZE
+OFF_BOTTOM_SCREEN = SCREEN_Y + SPRITE_SIZE
+FPS = 60
+
+MT_GAME_STUDIO_PALETTE = (b'\xf8\x1f\x00\x00\xcey\x00\xff\xf8\x1f\xff\x19\xfc\xe0\xfd\xe0'
+       b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
+
+SCORE_PALETTE = (b'\xf8\x1f\x00\x00\xcey\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+       b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
+
+# Using for button state
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released",
+}
